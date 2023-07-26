4634f7843a8bcac67df5cd4c6dc9b6dc97fc5983
kbledzki <krzysztofbledzki@gmail.com>
2023-07-26 15:00:55 +0200

    studnet entity and teacher

--- src/main/java/com/kb/java/University/entity/Student.java
+++ src/main/java/com/kb/java/University/entity/Student.java
@@ -1,4 +1,26 @@
 package com.kb.java.University.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = Student.TABLE_NAME)
 public class Student {
+    public static final String TABLE_NAME = "student";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
 }
+
+
--- src/main/java/com/kb/java/University/entity/Teacher.java
+++ src/main/java/com/kb/java/University/entity/Teacher.java
@@ -1,5 +1,24 @@
 package com.kb.java.University.entity;
 
-public class Teacher {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = Student.TABLE_NAME)
+public class Teacher {
+    public static final String TABLE_NAME = "teacher";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
 }
