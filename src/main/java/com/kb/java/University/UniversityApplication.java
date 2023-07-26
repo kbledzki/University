52f14e9cd34ef2134d2bb617dee0a68a1a04c849
kbledzki <krzysztofbledzki@gmail.com>
2023-07-26 13:01:44 +0200

    init commit, git ignore

---
+++ src/main/java/com/kb/java/University/UniversityApplication.java
@@ -0,0 +1,13 @@
+package com.kb.java.University;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
