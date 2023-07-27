131e307e5f1ed535c05c212729c28d632ad35df8
kbledzki <krzysztofbledzki@gmail.com>
2023-07-27 12:43:42 +0200

    grade service

--- src/main/java/com/kb/java/university/service/GradeService.java
+++ src/main/java/com/kb/java/university/service/GradeService.java
@@ -2,9 +2,14 @@ package com.kb.java.university.service;
 
 import com.kb.java.university.dto.GradeRequest;
 import com.kb.java.university.dto.GradeResponse;
+import com.kb.java.university.dto.StudentResponse;
 import com.kb.java.university.entity.Grade;
+import com.kb.java.university.entity.Student;
+import com.kb.java.university.exception.ObjectNotFoundException;
 import com.kb.java.university.repository.GradeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GradeService {
@@ -15,6 +20,13 @@ public class GradeService {
         this.gradeRepository = gradeRepository;
         this.studentService = studentService;
     }
+
+    public GradeResponse findGrade(Long id) {
+        Grade gradeById = getGradeById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Not found student with given id: " + id));
+        return new GradeResponse(gradeById.getId(), gradeById.getGradeValue(), gradeById.getStudent());
+    }
+
     public GradeResponse createGrade(GradeRequest gradeRequest){
         Grade grade = Grade.builder()
                 .gradeValue(gradeRequest.getGradeValue())
@@ -25,8 +37,12 @@ public class GradeService {
     }
 
     public void setGradeToStudent(Long id, String lastName){
+        GradeResponse gradeToSet = findGrade(id);
         studentService.findStudentByLastName(lastName);
+    }
 
+    private Optional<Grade> getGradeById(Long id) {
+        return gradeRepository.findById(id);
     }
 
 }
